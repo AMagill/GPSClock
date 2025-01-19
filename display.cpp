7e4b5e60fc55ad417f2c7d4a87b749cfc1b14f5e
Andrew Magill <AMagill@users.noreply.github.com>
2025-01-19 02:20:36 -0700

    Added support for v2 display board

--- display.cpp
+++ display.cpp
@@ -37,15 +37,19 @@ void Display::send_leds()
 
 void Display::set_brightness(uint8_t bright)
 {
-	// TODO: Balance brightness on each channel
+	// The TLC5952 supplies less current to the blue channels,
+	// so we need to adjust the brightness to compensate.
+	uint8_t brightRG = bright * 0.88;
+
 	m_control_buffer = 0x01'000000 |
-		bright << 14 |  // Bright B 0-127
-		bright <<  7 |  // Bright G 0-127
-		bright;         // Bright R 0-127
+		bright   << 14 |  // Bright B 0-127
+		brightRG <<  7 |  // Bright G 0-127
+		brightRG;         // Bright R 0-127
 }
 
 void Display::set_digit(uint digit, uint8_t value, bool dp)
 {
+#if BOARD_V1
 	static constexpr uint32_t dp_bits = 0x000001;
 	static constexpr std::array<uint32_t, 10> digit_bits = {
 		0x248248, // 0
@@ -64,11 +68,36 @@ void Display::set_digit(uint digit, uint8_t value, bool dp)
 	uint offset = digit % 3;
 	m_onoff_buffer[chip] &= ~((digit_bits[8] | dp_bits) << offset);
 	m_onoff_buffer[chip] |= (digit_bits[value] | (dp ? dp_bits : 0)) << offset;
+#else // BOARD_V2
+	static constexpr uint32_t dp_bit = 0x000001;
+	static constexpr std::array<uint8_t, 11> digit_bits = {
+		0xEE, // 0
+		0x82, // 1
+		0xDC, // 2
+		0xD6, // 3
+		0xB2, // 4
+		0x76, // 5
+		0x7E, // 6
+		0xC2, // 7
+		0xFE, // 8
+		0xF6, // 9
+		0x00
+	};
+
+	uint chip   = num_chips - 1 - (digit / 3);
+	uint offset = digit % 3;
+	m_onoff_buffer[chip] &= ~((0xFF << (offset * 8)));
+	m_onoff_buffer[chip] |= (digit_bits[value] | (dp ? dp_bit : 0)) << (offset * 8);
+#endif
 }
 
 void Display::set_colons(bool on)
 {
+#if BOARD_V1
 	constexpr uint32_t colon_bits = 0x000249;
+#else // BOARD_V2
+	constexpr uint32_t colon_bits = 0xFF;
+#endif
 	if (on)
 		m_onoff_buffer[5] |= colon_bits;
 	else
